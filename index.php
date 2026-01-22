b850b2b83409deaf27d6c0efb353b6e075568db7
catpatnova <Oskar.Vaht@gmail.com>
2026-01-22 09:33:20 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('./connection.php');
+
+$stmt = $pdo->query('SELECT title, id FROM books');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+
+<?php while ($book = $stmt->fetch()) { ?>
+        <li>
+            <a href="book.php?id=<?= $book['id'] ?>">
+                <?= $book['title']; ?>
+            </a>
+        </li>
+</body>
+</html>
+<?php } ?>
